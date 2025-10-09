7b5487c59c5b8e7f1d7c486a2329015328e3a2df
honam <75104291+honambsn@users.noreply.github.com>
2025-10-09 21:36:25 +0700

    wip villaapicontroller

--- MagicAPI/Controllers/VillaAPIController.cs
+++ MagicAPI/Controllers/VillaAPIController.cs
@@ -7,13 +7,18 @@ using Microsoft.AspNetCore.Mvc.Formatters;
 namespace MagicAPI.Controllers
 {
     [Route("api/VillaAPI")]
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
         public IEnumerable<VillaDTO> GetVillas() {
             return VillaStore.villaList;
+        }
+
+        [HttpGet("id")]
+        public VillaDTO GetVillaByID(int id) {
+            return VillaStore.villaList.FirstOrDefault(u => u.ID == id);
         }
     }
 }
